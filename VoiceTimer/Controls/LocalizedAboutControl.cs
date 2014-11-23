5af183082ed3837ee99b03a5d1ea390c960793f3
bsautermeister <apps@bsautermeister.de>
2014-11-24 00:27:28 +0100

    Updated tile icon

--- VoiceTimer/Controls/LocalizedAboutControl.cs
+++ VoiceTimer/Controls/LocalizedAboutControl.cs
@@ -1,18 +1,24 @@
 ﻿using VoiceTimer.Resources;
 using System;
+using PhoneKit.Framework.Controls;
+using System.Collections.Generic;
+using System.Windows.Media;
+using System.Windows;
 
 namespace VoiceTimer.Controls
 {
     /// <summary>
     /// The localized About control.
     /// </summary>
-    public class LocalizedAboutControl : ThemedAboutControlBase
+    public class LocalizedAboutControl : AboutControlBase
     {
         /// <summary>
         /// Localizes the user controls contents and texts.
         /// </summary>
         protected override void LocalizeContent()
         {
+            BackgroundTheme.Color = (Color)Application.Current.Resources["ThemeBackgroundMediumColor"];
+
             ApplicationIconSource = new Uri("/Assets/ApplicationIcon.png", UriKind.Relative);
             ApplicationTitle = AppResources.ApplicationTitle;
             ApplicationVersion = AppResources.ApplicationVersion;
@@ -25,6 +31,12 @@ namespace VoiceTimer.Controls
             RateAndReviewText = AppResources.RateAndReview;
             MoreAppsText= AppResources.MoreApps;
             MoreAppsSearchTerms = "Benjamin Sautermeister";
+
+            // contributors
+            ContributorsListVisibility = System.Windows.Visibility.Visible;
+            IList<ContributorModel> contributors = new List<ContributorModel>();
+            contributors.Add(new ContributorModel("/Assets/Images/icon.png", "D. Baranovskiy (The Noun Project)"));
+            SetContributorsList(contributors);
         }
     }
 }
